0a350a711c062b3af851db0c0a9eba98bde401f7
Frederic Lauzier <lauzier.frederic@gmail.com>
2020-11-09 18:09:56 -0500

    Functioning game, no point system yet

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,17 +1,38 @@
+require 'open-uri'
+require 'json'
+
 class GamesController < ApplicationController
 
-  @letters = GamesController.new(9)
-
-  def new(size)
+  def new
     counter = 0
     the_grid = []
-    until counter == size
+    until counter == 9
       the_grid << ('a'..'z').to_a.sample(1)
       counter += 1
     end
-    the_grid.flatten
+    @letters = the_grid.flatten
   end
 
   def score
+    @letters = params[:letters]
+    @attempt = params[:attempt]
+    url = "https://wagon-dictionary.herokuapp.com/#{@attempt}"
+    dictionary_serialized = open(url).read
+    dictionary = JSON.parse(dictionary_serialized)
+    if dictionary['found'] == true && @attempt.split('').all? { |letter| @attempt.count(letter) <= @letters.count(letter) }
+
+      @result = {
+        attempt: @attempt,
+        message: 'Good game good game',
+        score: 'to come soon'
+      }
+    else
+      @result =  {
+        attempt: @attempt,
+        message: 'Invalid word',
+        score: 0
+      }
+    end
+    @result
   end
 end
